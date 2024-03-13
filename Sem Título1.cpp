a68c1048c490b307a4d703ab738ff64889de1023
Fidelixjv <135646944+Fidelixjv@users.noreply.github.com>
2024-03-13 14:44:27 -0300

    Add files via upload

---
+++ Sem Título1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	
+	int n;
+	int v[5] = {n};
+	
+	for (int i = 0; i < 5; i++){
+	scanf("%d", &n);
+	}
+	
+	for (int i = 0; i < 5; i++){
+	if( n >= 3){
+		printf("%d", n);
+	}
+	}
+	return 0;
+}
